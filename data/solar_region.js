52db28f13351acc275ecfeed70ee592bdfd52d15
rsalcedocampino <rsalcedocampino@gmail.com>
2026-08-20 10:15:13 -0400

    Auto-publicar datos (2026-08-20 10:15) [tarea cada 2h]

---
+++ data/solar_region.js
@@ -0,0 +1,2 @@
+window.PW_DATA = window.PW_DATA || {};
+window.PW_DATA.solar_region = {"labels":["Antofagasta","Atacama","Metropolitana","Maule","Valparaíso","Coquimbo","Tarapacá","O'Higgins","Ñuble","Biobío","Arica y Parinacota","La Araucanía","Los Ríos","Aysén"],"mw":[4943.2,3079.4,1047.9,883.6,706.9,663.3,627.6,620.9,271.2,172.6,146.1,24.0,3.0,3.0]};
