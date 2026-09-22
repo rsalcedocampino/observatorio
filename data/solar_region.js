562e36e25373cfac1deccf7e7b5d61d912699f9e
rsalcedocampino <rsalcedocampino@gmail.com>
2026-09-21 22:48:19 -0300

    Auto-publicar datos (2026-09-21 22:48) [tarea cada 2h]

--- data/solar_region.js
+++ data/solar_region.js
@@ -1,2 +1,2 @@
 window.PW_DATA = window.PW_DATA || {};
-window.PW_DATA.solar_region = {"labels":["Antofagasta","Atacama","Metropolitana","Maule","Valparaíso","Coquimbo","Tarapacá","O'Higgins","Ñuble","Biobío","Arica y Parinacota","La Araucanía","Los Ríos","Aysén"],"mw":[5157.5,3079.4,1196.6,883.6,712.9,663.3,627.6,620.9,300.2,184.6,146.1,24.0,3.0,3.0]};
+window.PW_DATA.solar_region = {"labels":["Antofagasta","Atacama","Metropolitana","Maule","Valparaíso","Coquimbo","Tarapacá","O'Higgins","Ñuble","Biobío","Arica y Parinacota","La Araucanía","Los Ríos","Aysén"],"mw":[5157.5,3079.4,1202.9,892.6,715.6,660.3,627.6,605.9,303.9,180.9,146.1,24.0,3.0,3.0]};
